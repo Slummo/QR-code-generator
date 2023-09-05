ceccd14df89ab609f1d0ff64d966ba9631c255dd
Luca <marchisioluca15@gmail.com>
2023-09-05 18:07:41 +0200

    Added handler for requests

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const path = require("path");
+const qr = require("qrcode");
 
 const app = express();
 const port = 8888;
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/generate-qr-code", async (req, res) => {
+  try {
+    const url = req.query.url;
+
+    if (!url) {
+      return res.status(400).send("ERROR 404: Missing 'url' parameter!");
+    }
+
+    const qrCode = await qr.toDataURL(url);
+
+    res.send(`<img src="${qrCode}" alt="QR Code"/>`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("ERROR 500: Internal Server Error");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
